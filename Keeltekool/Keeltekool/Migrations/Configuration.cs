64185637ee33df00325551906e27f3c55001be2c
opilane <opilane@HP-CZC2349HTZ>
2026-01-28 11:35:15 +0200

    .

--- Keeltekool/Keeltekool/Migrations/Configuration.cs
+++ Keeltekool/Keeltekool/Migrations/Configuration.cs
@@ -1,6 +1,6 @@
 ﻿namespace Keeltekool.Migrations
 {
-    using Keeltekool.Models;
+    //using Keeltekool.Models;
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
     using System;
@@ -8,58 +8,58 @@
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Keeltekool.Models.ApplicationDbContext>
-    {
-        public Configuration()
-        {
-            AutomaticMigrationsEnabled = false;
-        }
+    //internal sealed class Configuration : DbMigrationsConfiguration<Keeltekool.Models.ApplicationDbContext>
+    //{
+    //    public Configuration()
+    //    {
+    //        AutomaticMigrationsEnabled = false;
+    //    }
 
-        protected override void Seed(Keeltekool.Models.ApplicationDbContext context)
-        {
-            var roleStore = new RoleStore<IdentityRole>(context);
-            var roleManager = new RoleManager<IdentityRole>(roleStore);
-            var userStore = new UserStore<ApplicationUser>(context);
-            var userManager = new UserManager<ApplicationUser>(userStore);
+    //    protected override void Seed(Keeltekool.Models.ApplicationDbContext context)
+    //    {
+    //        var roleStore = new RoleStore<IdentityRole>(context);
+    //        var roleManager = new RoleManager<IdentityRole>(roleStore);
+    //        var userStore = new UserStore<ApplicationUser>(context);
+    //        var userManager = new UserManager<ApplicationUser>(userStore);
 
 
 
-            string[] roles = { "Admin", "Opetaja", "Opilane" };
+    //        string[] roles = { "Admin", "Opetaja", "Opilane" };
 
-            foreach (var role in roles)
-            {
-                if (!roleManager.RoleExists(role))
-                {
-                    roleManager.Create(new IdentityRole(role));
-                }
-            }
-            var adminEmail = "Admin@kool.ee";
-            var adminUser = context.Users.FirstOrDefault(u => u.Email == adminEmail);
+    //        foreach (var role in roles)
+    //        {
+    //            if (!roleManager.RoleExists(role))
+    //            {
+    //                roleManager.Create(new IdentityRole(role));
+    //            }
+    //        }
+    //        var adminEmail = "Admin@kool.ee";
+    //        var adminUser = context.Users.FirstOrDefault(u => u.Email == adminEmail);
 
-            if (!context.Roles.Any(r => r.Name == "Admin"))
-            {
-                var role = new IdentityRole { Name = "Admin" };
-                roleManager.Create(role);
-            }
+    //        if (!context.Roles.Any(r => r.Name == "Admin"))
+    //        {
+    //            var role = new IdentityRole { Name = "Admin" };
+    //            roleManager.Create(role);
+    //        }
 
-            if (adminUser == null)
-            {
-                var user = new ApplicationUser { UserName = adminEmail, Email = adminEmail };
-                userManager.Create(user, "Admin_1234");
-                adminUser = user;
-            }
+    //        if (adminUser == null)
+    //        {
+    //            var user = new ApplicationUser { UserName = adminEmail, Email = adminEmail };
+    //            userManager.Create(user, "Admin_1234");
+    //            adminUser = user;
+    //        }
 
-            if (!userManager.IsInRole(adminUser.Id, "Admin"))
-            {
-                userManager.AddToRole(adminUser.Id, "Admin");
-            }
+    //        if (!userManager.IsInRole(adminUser.Id, "Admin"))
+    //        {
+    //            userManager.AddToRole(adminUser.Id, "Admin");
+    //        }
 
-            if (!context.Roles.Any(r => r.Name == "Admin"))
-            {
-                var role = new IdentityRole { Name = "Admin" };
-                roleManager.Create(role);
-            }
+    //        if (!context.Roles.Any(r => r.Name == "Admin"))
+    //        {
+    //            var role = new IdentityRole { Name = "Admin" };
+    //            roleManager.Create(role);
+    //        }
 
-        }
-    }
+    //    }
+    //}
 }
